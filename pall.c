25cf69dfdce6134b98bcbe3a7e34ef62ec25d536
sosthene <sothenetimi94@gmail.com>
2023-05-24 00:56:25 -0700

    printing all values in the stack

---
+++ pall.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * pall - prints all values in the stack
+ * @stack: double pointer to the stack whose values are to be printed
+ * @line_number: the line number containing monty opcode
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
