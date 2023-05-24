b2b556d8cee8315c6ecfb65e16dbf59f6d7928c3
sosthene <sothenetimi94@gmail.com>
2023-05-24 01:31:41 -0700

    adding the top two elements of a stack

---
+++ add.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * add - adds the top two elements of the stack together
+ * @stack: double pointer to the stack whose elements are added
+ * @line_number: line containg opcode in a monty file
+ */
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
