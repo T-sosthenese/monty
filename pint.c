8331b30535fd81ff6e690c47658e6d75d218800b
sosthene <sothenetimi94@gmail.com>
2023-05-24 01:03:43 -0700

    printing element at the top of the stack

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * pint - prints value at the top of the stack
+ * @stack: double pointer to stack whose value is to be printed
+ * @line_number: the line containing monty opcode
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
