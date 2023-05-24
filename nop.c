ea3e7fc154504781ad884c8079170cb62bcd81c9
sosthene <sothenetimi94@gmail.com>
2023-05-24 01:39:18 -0700

    no operation

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - used as placeholder of the monty byte code file
+ * @stack: double pointer to the stack
+ * @line_number: line number containing monty opcode
+ */
+/* No operation */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
