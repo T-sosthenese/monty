7f72a7f60f5bab73ae4e183694b94f70218109fb
sosthene <sothenetimi94@gmail.com>
2023-05-24 03:29:23 -0700

    running the opcode

---
+++ execute_opcode.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * execute_opcode - compares current opcode with builtins
+ * @stack: double pointer to the stack to be manipulated
+ * @str: string to compare opcode with builtins
+ * @line_number: the current line in opcode execution
+ *
+ * Return: nothing
+ */
+
+void execute_opcode(stack_t **stack, char *str, unsigned int line_number)
+{
+	int j = 0;
+
+	instruction_t op_code[] = INSTRUCTIONS;
+
+	if (!strcmp(str, "stack"))
+	{
+		global.data_struct = 1;
+		return;
+	}
+	while (op_code[j].opcode)
+	{
+		if (strcmp(op_code[j].opcode, str) == 0)
+		{
+			op_code[j].f(stack, line_number);
+			return;
+		}
+		j++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, str);
+	exit(EXIT_FAILURE);
+}
