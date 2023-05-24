5fd96ab4a558c551b7b56a84682c186b7663465c
sosthene <sothenetimi94@gmail.com>
2023-05-24 01:13:04 -0700

    deleting element at the top of a stack

---
+++ pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * pop - removes an element at the top of the stack
+ * @stack: points to the stack whose element is to be deleted
+ * @line_number: line number containg monty opcode
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	stack *temp = *stack;
+
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
+}
