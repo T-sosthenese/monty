7e040be598f91d94d88ae28a2ef2d542bf7088f6
sosthene <sothenetimi94@gmail.com>
2023-05-25 03:02:44 -0700

    removing the top element in a stack

--- pop.c
+++ pop.c
@@ -8,14 +8,15 @@
 
 void pop(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp;
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	stack *temp = *stack;
-
+	temp = *stack;
 	*stack = (*stack)->next;
 	if (*stack != NULL)
 		(*stack)->prev = NULL;
