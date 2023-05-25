56f9c3706c53ed3cd369e316ecd488e8199fb63a
sosthene <sothenetimi94@gmail.com>
2023-05-25 01:26:03 -0700

    displaying stack contents

---
+++ stack_printing.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * stack_printing - displays the contents of stack_t
+ * @stack: pointer to the stack to be printed
+ *
+ * Return: number of items in the stack
+ */
+
+size_t stack_printing(const stack_t *stack)
+{
+	size_t elem = 0;
+
+	while (stack != NULL)
+	{
+		printf("%d\n", stack->n);
+		stack = stack->next;
+		elem++;
+	}
+	return (elem);
+}
