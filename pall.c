7e7161d77006adce45d78770c0d784c0712968ed
sosthene <sothenetimi94@gmail.com>
2023-05-25 01:30:06 -0700

    printing all stack elements

--- pall.c
+++ pall.c
@@ -6,13 +6,7 @@
  * @line_number: the line number containing monty opcode
  */
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-	stack_t *current = *stack;
-
-	while (current != NULL)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
+	stack_printing(*stack);
 }
